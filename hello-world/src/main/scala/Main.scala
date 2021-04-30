88c9a28ab293b1b7a03a0781f489c623cfb3d93c
nero-15 <nero15.dev@gmail.com>
2021-04-30 23:22:08 +0900

    method

--- hello-world/src/main/scala/Main.scala
+++ hello-world/src/main/scala/Main.scala
@@ -24,9 +24,25 @@ object Main extends App {
     val addOne = (x: Int) => x + 1
     println(addOne(1)) // 2
 
-    val add = (x: Int, y: Int) => x + y
-    println(add(1, 2)) // 3
+    //val add = (x: Int, y: Int) => x + y
+    //println(add(1, 2)) // 3
 
     val getTheAnswer = () => 42
     println(getTheAnswer()) // 42
+
+    // method
+    def add(x: Int, y: Int): Int = x + y
+    println(add(1, 2)) // 3
+
+    def addThenMultiply(x: Int, y: Int)(multiplier: Int): Int = (x + y) * multiplier
+    println(addThenMultiply(1, 2)(3)) // 9
+
+    def name: String = System.getProperty("user.name")
+    println("Hello, " + name + "!")
+
+    def getSquareString(input: Double): String = {
+      val square = input * input
+      square.toString
+    }
+    println(getSquareString(2.5)) // 6.25
 }
