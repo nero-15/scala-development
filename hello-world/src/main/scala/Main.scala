236f315658c59e3379d7c18ec77f0d100a7ffae0
nero-15 <nero15.dev@gmail.com>
2021-04-30 23:29:18 +0900

    case class

--- hello-world/src/main/scala/Main.scala
+++ hello-world/src/main/scala/Main.scala
@@ -54,4 +54,22 @@ object Main extends App {
 
     val greeter = new Greeter("Hello, ", "!")
     greeter.greet("Scala developer") // Hello, Scala developer!
+
+    // case class
+    case class Point(x: Int, y: Int)
+    val point = Point(1, 2)
+    val anotherPoint = Point(1, 2)
+    val yetAnotherPoint = Point(2, 2)
+
+    if (point == anotherPoint) {
+      println(point + " と " + anotherPoint + " は同じです。")
+    } else {
+      println(point + " と " + anotherPoint + " は異なります。")
+    } // Point(1,2) と Point(1,2) は同じです。
+
+    if (point == yetAnotherPoint) {
+      println(point + " と " + yetAnotherPoint + " は同じです。")
+    } else {
+      println(point + " と " + yetAnotherPoint + " は異なります。")
+    } // Point(1,2) と Point(2,2) は異なります。
 }
