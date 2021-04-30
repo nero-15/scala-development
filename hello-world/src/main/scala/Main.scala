5649b784a204509cfcf5ae6d98f2e9dda0a7a03e
nero-15 <nero15.dev@gmail.com>
2021-04-30 23:02:52 +0900

    Expressions

--- hello-world/src/main/scala/Main.scala
+++ hello-world/src/main/scala/Main.scala
@@ -1,3 +1,16 @@
 object Main extends App {
-  println("Hello, Yokohama!")
+    println(1) // 1
+    println(1 + 1) // 2
+    println("Hello!") // Hello!
+    println("Hello," + " world!") // Hello, world!
+
+    //val x = 1 + 1
+    //println(x)
+
+    val x: Int = 1 + 1
+    println(x)
+
+    var v = 1 + 1
+    v = 3 // "x"は"var"キーワードで宣言されているので、これはコンパイルされます。
+    println(v * v) // 9
 }
