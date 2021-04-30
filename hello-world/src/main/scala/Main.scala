40428b2c5ecf7e9deaba8008d897ad64ef498049
nero-15 <nero15.dev@gmail.com>
2021-04-30 23:10:50 +0900

    function

--- hello-world/src/main/scala/Main.scala
+++ hello-world/src/main/scala/Main.scala
@@ -13,4 +13,20 @@ object Main extends App {
     var v = 1 + 1
     v = 3 // "x"は"var"キーワードで宣言されているので、これはコンパイルされます。
     println(v * v) // 9
+
+    // block
+    println({
+        val x = 1 + 1
+        x + 1
+    }) // 3
+
+    // function
+    val addOne = (x: Int) => x + 1
+    println(addOne(1)) // 2
+
+    val add = (x: Int, y: Int) => x + y
+    println(add(1, 2)) // 3
+
+    val getTheAnswer = () => 42
+    println(getTheAnswer()) // 42
 }
