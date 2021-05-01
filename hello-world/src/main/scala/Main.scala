06285756afec1b36f831ec885862394d6ee6a9b7
nero-15 <nero15.dev@gmail.com>
2021-05-01 17:21:10 +0900

    setter and getter

--- hello-world/src/main/scala/Main.scala
+++ hello-world/src/main/scala/Main.scala
@@ -126,6 +126,7 @@ object Main extends App {
     class User
     val user1 = new User
 
+    /*
     class Point(var x: Int = 0, var y: Int = 0)
 
     val origin = new Point  // x と y には共に0がセットされます。
@@ -133,6 +134,29 @@ object Main extends App {
     println(point1.x)  // 1 が出力されます。
     val point2 = new Point(y=2)
     println(point2.y)  // 2 が出力されます。
+    */
+
+    class Point {
+      private var _x = 0
+      private var _y = 0
+      private val bound = 100
+
+      def x = _x
+      def x_= (newValue: Int): Unit = {
+        if (newValue < bound) _x = newValue else printWarning
+      }
+
+      def y = _y
+      def y_= (newValue: Int): Unit = {
+        if (newValue < bound) _y = newValue else printWarning
+      }
+
+      private def printWarning = println("WARNING: Out of bounds")
+    }
+
+    val point1 = new Point
+    point1.x = 99
+    point1.y = 101 // 警告が出力されます。
 
 
 }
