2a9cddc77b0a5d31b42e74b3704ea7bb02ad1a62
nero-15 <nero15.dev@gmail.com>
2021-05-01 17:36:07 +0900

    sub typing

--- hello-world/src/main/scala/Main.scala
+++ hello-world/src/main/scala/Main.scala
@@ -182,5 +182,22 @@ object Main extends App {
     println(iterator.next())  // returns 0
     println(iterator.next())  // returns 1
 
+    import scala.collection.mutable.ArrayBuffer
+
+    trait Pet {
+      val name: String
+    }
+
+    class Cat(val name: String) extends Pet
+    class Dog(val name: String) extends Pet
+
+    val dog = new Dog("Harry")
+    val cat = new Cat("Sally")
+
+    val animals = ArrayBuffer.empty[Pet]
+    animals.append(dog)
+    animals.append(cat)
+    animals.foreach(pet => println(pet.name))  // Prints Harry Sally
+
 
 }
