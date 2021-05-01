3fa8a415a3e0db0a2e576d5650e4db115db4eeb9
nero-15 <nero15.dev@gmail.com>
2021-05-01 17:10:15 +0900

    class

--- hello-world/src/main/scala/Main.scala
+++ hello-world/src/main/scala/Main.scala
@@ -123,4 +123,23 @@ object Main extends App {
     val face: Char = '☺'
     val number: Int = face  // 9786
 
+    class User
+    val user1 = new User
+
+    class Point(var x: Int, var y: Int) {
+
+      def move(dx: Int, dy: Int): Unit = {
+        x = x + dx
+        y = y + dy
+      }
+
+      override def toString: String =
+        s"($x, $y)"
+    }
+
+    val point1 = new Point(2, 3)
+    point1.x  // 2
+    println(point1)  // prints (2, 3)
+
+
 }
