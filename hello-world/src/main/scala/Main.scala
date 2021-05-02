59de97087f2d326a65dcb064bbdb0bcc3f505300
nero-15 <nero15.dev@gmail.com>
2021-05-02 21:10:53 +0900

    nest

--- hello-world/src/main/scala/Main.scala
+++ hello-world/src/main/scala/Main.scala
@@ -286,6 +286,17 @@ object Main extends App {
     val url = getURL(endpoint, query) // "https://www.example.com/users?id=1": String
     println(url)
 
+    def factorial(x: Int): Int = {
+       def fact(x: Int, accumulator: Int): Int = {
+         if (x <= 1) accumulator
+         else fact(x - 1, x * accumulator)
+       }
+       fact(x, 1)
+    }
+
+    println("Factorial of 2: " + factorial(2))
+    println("Factorial of 3: " + factorial(3))
+
 
 
 }
