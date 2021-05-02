efac601408f3ad4826c53c04ee48105e6c065008
nero-15 <nero15.dev@gmail.com>
2021-05-02 21:55:26 +0900

    generic class

--- hello-world/src/main/scala/Main.scala
+++ hello-world/src/main/scala/Main.scala
@@ -426,5 +426,23 @@ object Main extends App {
 
     twentySomethings.foreach(name => println(name))  // prints Travis Dennis
 
+    class Stack[A] {
+      private var elements: List[A] = Nil
+      def push(x: A): Unit =
+        elements = x :: elements
+      def peek: A = elements.head
+      def pop(): A = {
+        val currentTop = peek
+        elements = elements.tail
+        currentTop
+      }
+    }
+
+    val stack = new Stack[Int]
+    stack.push(1)
+    stack.push(2)
+    println(stack.pop)  // prints 2
+    println(stack.pop)  // prints 1
+
 
 }
