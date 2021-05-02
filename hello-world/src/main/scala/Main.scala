1592a06296f205ecfd673dbfbd0bd689d9b9d153
nero-15 <nero15.dev@gmail.com>
2021-05-02 21:06:55 +0900

    callback

--- hello-world/src/main/scala/Main.scala
+++ hello-world/src/main/scala/Main.scala
@@ -198,7 +198,7 @@ object Main extends App {
     animals.append(dog)
     animals.append(cat)
     animals.foreach(pet => println(pet.name))  // Prints Harry Sally
-    */
+
 
     val ingredient = ("Sugar" , 25)
     println(ingredient._1) // Sugar
@@ -259,8 +259,32 @@ object Main extends App {
     }
 
     class RichStringIter extends StringIterator("Scala") with RichIterator
-val richStringIter = new RichStringIter
-richStringIter foreach println
+    val richStringIter = new RichStringIter
+    richStringIter foreach println
+    */
+
+    val salaries = Seq(20000, 70000, 40000)
+    val doubleSalary = (x: Int) => x * 2
+    val newSalaries = salaries.map(doubleSalary) // List(40000, 140000, 80000)
+
+    case class WeeklyWeatherForecast(temperatures: Seq[Double]) {
+
+      private def convertCtoF(temp: Double) = temp * 1.8 + 32
+
+      def forecastInFahrenheit: Seq[Double] = temperatures.map(convertCtoF) // <-- convertCtoFメソッドが渡されます
+    }
+
+    def urlBuilder(ssl: Boolean, domainName: String): (String, String) => String = {
+      val schema = if (ssl) "https://" else "http://"
+      (endpoint: String, query: String) => s"$schema$domainName/$endpoint?$query"
+    }
+
+    val domainName = "www.example.com"
+    def getURL = urlBuilder(ssl=true, domainName)
+    val endpoint = "users"
+    val query = "id=1"
+    val url = getURL(endpoint, query) // "https://www.example.com/users?id=1": String
+    println(url)
 
 
 
