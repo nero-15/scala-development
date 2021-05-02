23e990838ea8b58a2648c25d52d8158c21a6ba34
nero-15 <nero15.dev@gmail.com>
2021-05-02 21:36:07 +0900

    Pattern matching

--- hello-world/src/main/scala/Main.scala
+++ hello-world/src/main/scala/Main.scala
@@ -326,5 +326,49 @@ object Main extends App {
     println(message5.recipient) // claire@bourgogne.fr
     println(message5.body)  // "Me zo o komz gant ma amezeg"
 
+    import scala.util.Random
+
+    val x: Int = Random.nextInt(10)
+
+    x match {
+      case 0 => "zero"
+      case 1 => "one"
+      case 2 => "two"
+      case _ => "other"
+    }
+
+    def matchTest(x: Int): String = x match {
+      case 1 => "one"
+      case 2 => "two"
+      case _ => "other"
+    }
+    println(matchTest(3))  // other
+    println(matchTest(1))  // one
+
+    abstract class Notification
+
+    case class Email(sender: String, title: String, body: String) extends Notification
+
+    case class SMS(caller: String, message: String) extends Notification
+
+    case class VoiceRecording(contactName: String, link: String) extends Notification
+
+    def showNotification(notification: Notification): String = {
+      notification match {
+        case Email(sender, title, _) =>
+          s"You got an email from $sender with title: $title"
+        case SMS(number, message) =>
+          s"You got an SMS from $number! Message: $message"
+        case VoiceRecording(name, link) =>
+          s"you received a Voice Recording from $name! Click the link to hear it: $link"
+      }
+    }
+    val someSms = SMS("12345", "Are you there?")
+    val someVoiceRecording = VoiceRecording("Tom", "voicerecording.org/id/123")
+
+    println(showNotification(someSms))  // You got an SMS from 12345! Message: Are you there? が出力されます。
+
+    println(showNotification(someVoiceRecording))  // you received a Voice Recording from Tom! Click the link to hear it: voicerecording.org/id/123 が出力されます。
+
 
 }
