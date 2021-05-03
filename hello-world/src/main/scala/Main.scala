30824345c8f71985b142c875cd2fa7c64de1ce54
nero-15 <nero15.dev@gmail.com>
2021-05-03 21:53:40 +0900

    polymorphic methods

--- hello-world/src/main/scala/Main.scala
+++ hello-world/src/main/scala/Main.scala
@@ -558,6 +558,15 @@ object Main extends App {
     }
     */
 
+    def listOfDuplicates[A](x: A, length: Int): List[A] = {
+      if (length < 1)
+        Nil
+      else
+        x :: listOfDuplicates(x, length - 1)
+    }
+    println(listOfDuplicates[Int](3, 4))  // List(3, 3, 3, 3)
+    println(listOfDuplicates("La", 8))  // List(La, La, La, La, La, La, La, La)
+
 
 
 }
