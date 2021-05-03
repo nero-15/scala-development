77ee9b76fcd8fb7d772309b6a7728676b74d0f24
nero-15 <nero15.dev@gmail.com>
2021-05-03 21:20:33 +0900

    compound types

--- hello-world/src/main/scala/Main.scala
+++ hello-world/src/main/scala/Main.scala
@@ -500,6 +500,21 @@ object Main extends App {
     println("length = " + buf.length)
     println("content = " + buf.element)
 
+    trait Cloneable extends java.lang.Cloneable {
+      override def clone(): Cloneable = {
+        super.clone().asInstanceOf[Cloneable]
+      }
+    }
+    trait Resetable {
+      def reset: Unit
+    }
+
+    def cloneAndReset(obj: Cloneable with Resetable): Cloneable = {
+      val cloned = obj.clone()
+      obj.reset
+      cloned
+    }
+
 
 
 }
