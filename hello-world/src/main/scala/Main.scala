7769a548e87f6c9b93f4bc18276fe6ab648c4578
nero-15 <nero15.dev@gmail.com>
2021-05-03 21:34:08 +0900

    implicit parameters

--- hello-world/src/main/scala/Main.scala
+++ hello-world/src/main/scala/Main.scala
@@ -448,7 +448,7 @@ object Main extends App {
     // https://docs.scala-lang.org/ja/tour/variances.html
     // https://docs.scala-lang.org/ja/tour/upper-type-bounds.html
     // https://docs.scala-lang.org/ja/tour/lower-type-bounds.html
-    */
+
 
     class Graph {
       class Node {
@@ -530,6 +530,33 @@ object Main extends App {
 
     val realBeyoncé = new VerifiedTweeter("Beyoncé")
     realBeyoncé.tweet("Just spilled my glass of lemonade")  // "real Beyoncé: Just spilled my glass of lemonade"と出力します。
+
+    abstract class Monoid[A] {
+      def add(x: A, y: A): A
+      def unit: A
+    }
+
+    object ImplicitTest {
+      implicit val stringMonoid: Monoid[String] = new Monoid[String] {
+        def add(x: String, y: String): String = x concat y
+        def unit: String = ""
+      }
+
+      implicit val intMonoid: Monoid[Int] = new Monoid[Int] {
+        def add(x: Int, y: Int): Int = x + y
+        def unit: Int = 0
+      }
+
+      def sum[A](xs: List[A])(implicit m: Monoid[A]): A =
+        if (xs.isEmpty) m.unit
+        else m.add(xs.head, sum(xs.tail))
+
+      def main(args: Array[String]): Unit = {
+        println(sum(List(1, 2, 3)))       // intMonoidを暗に使用
+        println(sum(List("a", "b", "c"))) // stringMonoidを暗に使用
+      }
+    }
+    */
 
 
 
