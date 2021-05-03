ce3ad8e3af27e225aa494273c1447882cc923aa5
nero-15 <nero15.dev@gmail.com>
2021-05-03 21:11:21 +0900

    inner class

--- hello-world/src/main/scala/Main.scala
+++ hello-world/src/main/scala/Main.scala
@@ -450,6 +450,31 @@ object Main extends App {
     // https://docs.scala-lang.org/ja/tour/lower-type-bounds.html
     */
 
+    class Graph {
+      class Node {
+        var connectedNodes: List[Node] = Nil
+        def connectTo(node: Node) {
+          if (!connectedNodes.exists(node.equals)) {
+            connectedNodes = node :: connectedNodes
+          }
+        }
+      }
+      var nodes: List[Node] = Nil
+      def newNode: Node = {
+        val res = new Node
+        nodes = res :: nodes
+        res
+      }
+    }
+
+    val graph1: Graph = new Graph
+    val node1: graph1.Node = graph1.newNode
+    val node2: graph1.Node = graph1.newNode
+    node1.connectTo(node2)      // legal
+    val graph2: Graph = new Graph
+    val node3: graph2.Node = graph2.newNode
+    //node1.connectTo(node3)      // illegal!
+
 
 
 
